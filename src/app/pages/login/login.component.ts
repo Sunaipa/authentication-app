31c354f4e339e3d61e27c9161f5b147f7cd419d0
André Fernandes <andref.pro@gmail.com>
2022-01-05 10:30:06 +0100

    authent + redirection en cas erreur

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
+import { AuthenticationService } from './../../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { CredentialInterface } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userInput: CredentialInterface = {
+    login: '',
+    password: ''
+  };
+
+  isAuthenticated = false;
+  isSubmitted = false;
+
+  constructor(private security: AuthenticationService, 
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  validateForm(): void {
+    this.isSubmitted = true;
+    this.isAuthenticated = this.security.authenticate(this.userInput);
+    if (this.isAuthenticated) {
+      this.router.navigate(['/home'])
+    }else{
+      console.log('KO');
+    }
   }
 
 }
