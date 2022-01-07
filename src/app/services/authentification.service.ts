3e95052d7f715c363a2c59b5058bd116f977e29b
André Fernandes <andref.pro@gmail.com>
2022-01-07 10:55:22 +0100

    authentification persistante

--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { User, UserDataInterface, UserInterface } from '../models/user.model';
 import { NotificationService } from './notification.service';
+import { LocalStorageService } from 'ngx-webstorage';
 
 export interface CredentialInterface {
   login: string;
   password: string;
 }
+
+const USER_KEY = 'user';
 
 @Injectable({
   providedIn: 'root'
@@ -22,8 +25,16 @@ export class AuthentificationService {
 
   ]
 
-  constructor(private notification: NotificationService, private router: Router) {
-    this.user = new User();
+  constructor(  private notification: NotificationService, 
+                private router: Router,
+                private storage: LocalStorageService) {
+
+    const user = JSON.parse(storage.retrieve(USER_KEY));
+    if (user) {
+      this.user = new User(user)
+    } else {
+      this.user = new User();
+    }
    }
 
    findUser(credentials: CredentialInterface): UserDataInterface | undefined {
@@ -40,14 +51,16 @@ export class AuthentificationService {
     if (isAuthenticated) {
       this.notification.setMessage('Vous êtes connecté');
       this.user = new User(user);
+      this.storage.store(USER_KEY, JSON.stringify(this.user));
     }
     return isAuthenticated;
   }
 
   logout(): void {
     this.user = new User();
-    this.notification.setMessage('Vous allez être déconnecter, redirection en cours... ')
+    this.notification.setMessage('Vous allez être déconnecter, redirection en cours... ');
     this.router.navigate(['/home']);
+    this.storage.clear(USER_KEY);
   }
 
   
